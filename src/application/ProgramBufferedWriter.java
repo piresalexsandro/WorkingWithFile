5953e865b444efa9b575953c50aedff35b5f109d
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-01-08 18:38:33 -0200

    Buffered Create and Writer

---
+++ src/application/ProgramBufferedWriter.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ProgramBufferedWriter {
+
+	public static void main(String[] args) {
+		String[] lines = new String[] { "Writing good morning", "Writing good afternoon",  "Writing good night"};
+		String path = "c:\\temp\\out.txt";
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		
+	}
+
+}
