76156aba3d0e6a2550f3360db88b17772bfd0df2
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-01-08 22:26:29 -0200

    Trabalhando com Arquivos

---
+++ src/application/ProgramManipulatingFolder.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ProgramManipulatingFolder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		File path = new File(strPath);
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		// arquivos
+		File[] files = path.listFiles(File::isFile);
+		System.out.print("FILES: ");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		//subpasta
+		boolean success = new File(strPath + "\\subpasta").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		sc.close();
+	}
+
+}
